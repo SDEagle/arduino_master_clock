f9d8d78d2e8efdededc7dd1c7e3c80e6a6daf9fe
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2016-11-04 16:49:22 +0100

    implement logic for stepping through a song list

--- arduino_master_clock.ino
+++ arduino_master_clock.ino
@@ -1,8 +1,6 @@
-#include "MIDIUSB.h"
-
 #define BOUNCE_LOCK_OUT
 #include <Bounce2.h>
-
+#include "MIDIUSB.h"
 #include <TimerOne.h>
 
 #define PORT_START_STOP 2
@@ -10,14 +8,18 @@
 #define PORT_PREV 4
 #define BOUNCE_TIME 5
 
-boolean running = false;
-
 Bounce start_stop_bouncer = Bounce();
 Bounce next_bouncer = Bounce();
 Bounce prev_bouncer = Bounce();
 
-// Beats per thousand minitues, BPM with three decimal places
-unsigned long bptm = 60000;
+boolean running = false;
+
+// In beats per thousand minutes, BPM with three decimal places
+typedef unsigned long tempo;
+int current_song_index = 0;
+int current_track_selection = 0;
+tempo song_tempos[] = {180000, 140000, 143000, 150000, 162000, 111000, 160000};
+#define song_count() (sizeof(song_tempos) / sizeof(tempo))
 
 void setup() {
   pinMode(PORT_START_STOP, INPUT);
@@ -33,8 +35,8 @@ void setup() {
   next_bouncer.interval(BOUNCE_TIME);
   prev_bouncer.interval(BOUNCE_TIME);
 
-  Timer1.initialize(calculateIntervalMicroSecs(bptm));
-  Timer1.attachInterrupt(sendClockPulse);
+  Timer1.initialize(calculateIntervalMicroSecs(song_tempos[current_song_index]));
+  Timer1.attachInterrupt(sendClockPulseEvent);
 }
 
 void loop() {
@@ -55,70 +57,111 @@ void loop() {
 
 void onStartStopPressed() {
   if (running) {
-    sendStop();
+    sendStopEvent();
   } else {
-    sendStart();
+    sendStartEvent();
   }
   running = !running;
 }
 
 void onNextPressed() {
-  if (running) {
-  } else {
-  }
+  change_song(1);
 }
 
 void onPrevPressed() {
+  change_song(-1);
+}
+
+void sendStartEvent() {
+  Timer1.stop();
+  createStartEvent();
+  MidiUSB.flush();
+  Timer1.restart();
+}
+
+void sendStopEvent() {
+  createStopEvent();
+  MidiUSB.flush();
+}
+
+void change_song(int offset) {
   if (running) {
+    Timer1.stop();
+    createStopEvent();
+    current_song_index = (current_song_index + offset) % song_count();
+    updateTimer();
+    updateTrackSelection();
+    createStartEvent();
+    MidiUSB.flush();
+    Timer1.restart();
   } else {
+    current_song_index = (current_song_index + offset) % song_count();
+    updateTimer();
+    updateTrackSelection();
+    MidiUSB.flush();
   }
 }
 
 void updateTimer() {
-  Timer1.setPeriod(calculateIntervalMicroSecs(bptm));
+  Timer1.setPeriod(calculateIntervalMicroSecs(song_tempos[current_song_index]));
 }
 
-unsigned long calculateIntervalMicroSecs(unsigned long bptm) {
+unsigned long calculateIntervalMicroSecs(tempo bptm) {
   // 60 * 1000 * 1000 * 1000 / 24 (PULSES PER BEAT) /bptm
   return 2500000000ul / bptm;
 }
 
-void sendClockPulse() {
+void updateTrackSelection() {
+  int offset = current_song_index - current_track_selection;
+  while(offset > 0) {
+    createCCEventForNextTrack();
+    offset--;
+  }
+  while(offset < 0) {
+    createCCEventForPrevTrack();
+    offset++;
+  }
+}
+
+void sendClockPulseEvent() {
+  createClockPulseEvent();
+  MidiUSB.flush();
+}
+
+
+void createClockPulseEvent() {
   uint8_t data[2];
   data[0] = 0x0F;
   data[1] = 0xF8;
   MidiUSB.write(data, 2);
-  MidiUSB.flush();
 }
 
-void sendStart() {
+void createStartEvent() {
   uint8_t data[2];
   data[0] = 0x0F;
   data[1] = 0xFA;
   MidiUSB.write(data, 2);
-  MidiUSB.flush();
 }
 
-void sendStop() {
+void createStopEvent() {
   uint8_t data[2];
   data[0] = 0x0F;
   data[1] = 0xFC;
   MidiUSB.write(data, 2);
-  MidiUSB.flush();
 }
 
-// First parameter is the event type (0x09 = note on, 0x08 = note off).
-// Second parameter is note-on/note-off, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the note number (48 = middle C).
-// Fourth parameter is the velocity (64 = normal, 127 = fastest).
-
-void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOn);
+void createCCEventForNextTrack() {
+  createControlChangeEvent(0, 80, 127);
+  createControlChangeEvent(0, 80, 0);
 }
 
-void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOff);
+void createCCEventForPrevTrack() {
+  createControlChangeEvent(0, 81, 127);
+  createControlChangeEvent(0, 81, 0);
 }
+
+void createControlChangeEvent(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+
